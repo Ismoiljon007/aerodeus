5fca4f7a951fddf7299345a265f66e6475a8ee3c
Ismoiljon007 <ibahtiyorov007@gmail.com>
2026-01-02 04:48:30 +0500

    update async data fetching to include locale in keys

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -22,15 +22,15 @@
 <script setup lang="ts">
 const { locale } = useI18n();
 
-const { data: hero } = await useAsyncData('hero', () => apiFetch('/hero/', { params: { lang: locale.value } }), { watch: [locale] });
-const { data: partners } = await useAsyncData('partners', () => apiFetch('/partners/', { params: { lang: locale.value } }), { watch: [locale] });
-const { data: advantages } = await useAsyncData('advantages', () => apiFetch('/advantages/', { params: { lang: locale.value } }), { watch: [locale] });
-const { data: services } = await useAsyncData('services', () => apiFetch('/services/with-features/', { params: { lang: locale.value } }), { watch: [locale] });
-const { data: aircrcraft } = await useAsyncData('aircrcraft', () => apiFetch('/aircraft/', { params: { lang: locale.value } }), { watch: [locale] });
-const { data: howWeWork } = await useAsyncData('howWeWork', () => apiFetch('/how-we-work/section/', { params: { lang: locale.value } }), { watch: [locale] });
-const { data: howWeWorkSteps } = await useAsyncData('howWeWorkSteps', () => apiFetch('/how-we-work/steps/', { params: { lang: locale.value } }), { watch: [locale] });
-const { data: statistics } = await useAsyncData('statistics', () => apiFetch('/statistics/', { params: { lang: locale.value } }), { watch: [locale] });
-const { data: about } = await useAsyncData('about', () => apiFetch('/about/', { params: { lang: locale.value } }), { watch: [locale] });
+const { data: hero } = await useAsyncData(() => `hero-${locale.value}`, () => apiFetch('/hero/', { params: { lang: locale.value } }), { watch: [locale] });
+const { data: partners } = await useAsyncData(() => `partners-${locale.value}`, () => apiFetch('/partners/', { params: { lang: locale.value } }), { watch: [locale] });
+const { data: advantages } = await useAsyncData(() => `advantages-${locale.value}`, () => apiFetch('/advantages/', { params: { lang: locale.value } }), { watch: [locale] });
+const { data: services } = await useAsyncData(() => `services-${locale.value}`, () => apiFetch('/services/with-features/', { params: { lang: locale.value } }), { watch: [locale] });
+const { data: aircrcraft } = await useAsyncData(() => `aircrcraft-${locale.value}`, () => apiFetch('/aircraft/', { params: { lang: locale.value } }), { watch: [locale] });
+const { data: howWeWork } = await useAsyncData(() => `howWeWork-${locale.value}`, () => apiFetch('/how-we-work/section/', { params: { lang: locale.value } }), { watch: [locale] });
+const { data: howWeWorkSteps } = await useAsyncData(() => `howWeWorkSteps-${locale.value}`, () => apiFetch('/how-we-work/steps/', { params: { lang: locale.value } }), { watch: [locale] });
+const { data: statistics } = await useAsyncData(() => `statistics-${locale.value}`, () => apiFetch('/statistics/', { params: { lang: locale.value } }), { watch: [locale] });
+const { data: about } = await useAsyncData(() => `about-${locale.value}`, () => apiFetch('/about/', { params: { lang: locale.value } }), { watch: [locale] });
 </script>
 
 <style scoped>
